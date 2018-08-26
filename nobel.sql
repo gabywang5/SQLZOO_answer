3e14c892b60294eaa2dff82d709c43096593ce2b
meiqin <seraphwang93@gmail.com>
2018-08-26 15:50:27 +0800

    modify the nobel.sql

--- nobel.sql
+++ nobel.sql
@@ -15,7 +15,12 @@ ORDER BY yr DESC, winner;
 SELECT winner, subject
   FROM nobel
  WHERE yr=1984
- ORDER BY subject IN ('Physics','Chemistry'),subject,winner;
+ ORDER BY 
+ CASE WHEN subject IN ('Physics','Chemistry') 
+      THEN 1 
+      ELSE 0 
+      END,
+      subject, winner;
 
 SELECT yr
 FROM nobel
